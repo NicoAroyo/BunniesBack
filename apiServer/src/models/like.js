799f71fdf2827d3e2c9522ef15af0503453c8080
TsviBekker <bekker.tsvii@gmail.com>
2022-09-30 13:00:24 +0300

    made auth server

--- apiServer/src/models/like.js
+++ apiServer/src/models/like.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const likeSchema = new mongoose.Schema({
   userId: {
-    required : true,
-    type : String,
-  }, 
-  postId : {
-    required : true, 
-    type : String
-  }
+    required: true,
+    type: String,
+  },
+  postId: {
+    required: true,
+    type: String,
+  },
 });
 
 export default mongoose.model("Like", likeSchema);
