06a0aef7d81ecde9d6b9f5ad87e1c508ef7538c3
NicoAroyo <nico.aroyo@gmail.com>
2022-10-03 16:46:57 +0300

    reject

--- apiServer/src/routes/relationships.js
+++ apiServer/src/routes/relationships.js
@@ -105,21 +105,24 @@ relationshipsRouter.patch(
   async (req, res) => {
     try {
       const { senderId } = req.params;
-      const { receiver } = req.body;
+      const receiverId = req.body._id;
       const sender = await User.findById(senderId);
+      const receiver = await User.findById(receiverId);
 
       //UPDATE SENDER - REQUESTS SENT
+      const newRequestsSent = sender.requestsSent.filter(
+        (req) => req === receiver._id
+      );
       await User.findByIdAndUpdate(sender._id, {
-        ...sender,
-        requestsSent: sender.requestsSent.filter((req) => req !== receiver._id),
+        requestsSent: newRequestsSent,
       });
 
       //UPDATE RECEIVER - REQUESTS RECEIVED, FRIENDS
+      const newRequestsReceived = receiver.requestsReceived.filter(
+        (req) => req === sender._id
+      );
       const updatedReceiver = await User.findByIdAndUpdate(receiver._id, {
-        ...receiver,
-        requestsReceived: receiver.requestsReceived.filter(
-          (req) => req !== sender._id
-        ),
+        requestsReceived: newRequestsReceived,
       });
       res.status(201).json({ user: updatedReceiver });
     } catch (error) {
